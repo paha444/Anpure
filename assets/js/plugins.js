9004cf882c96058acb43e856fc0deeca0056721d
Lighty <kh.ultra@gmail.com>
2020-09-09 18:08:46 +0300

    0

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,3 +1,11 @@
-
-$(document).pjax('a, .menu-item a, .logo a', '.pjax-container');
-//  всю страницу в html требуется обернуть в pjax-container (".dynamic-loader")
+function setNavigation() {
+     let current_location = location.pathname.split('/')[1];
+     if (current_location === "") return;
+     let menu_items = document.querySelector(".nav-bar").getElementsByTagName("a");
+     for (let i = 0, len = menu_items.length; i < len; i++) {
+          if (menu_items[i].getAttribute("href").indexOf(current_location) !== -1) {
+               menu_items[i].className = "active-href";
+          }
+     }
+}
+setNavigation();
